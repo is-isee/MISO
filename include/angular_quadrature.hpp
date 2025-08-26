a6f71af39e766017091304bb14e665d3d3e91da4
iijimahr <haruhisa.iijima@dummy.email.com>
2025-08-26 11:11:34 +0900

    implemented angular quadrature class

---
+++ include/angular_quadrature.hpp
@@ -0,0 +1,62 @@
+///
+/// @brief Angular quadrature for radiative transfer calculations
+///
+#pragma once
+
+#include <cmath>
+#include <stdexcept>
+#include <vector>
+
+template <class Real> struct AngularQuadrature {
+  /// @brief Number of angles
+  const int num_angles;
+
+  /// @brief Angle weights
+  std::vector<Real> weights;
+
+  /// @brief Angle directions
+  std::vector<Real> mu_x, mu_y, mu_z;
+
+  AngularQuadrature(const int num_angles)
+      : num_angles(num_angles), weights(num_angles), mu_x(num_angles),
+        mu_y(num_angles), mu_z(num_angles) {
+    switch (num_angles) {
+    case 24:
+      // Carlson's A4 quadrature
+      compute_quadrature_carlson_a4();
+      break;
+    case 1:
+      // Single ray in positive-z direction
+      compute_quadrature_single_positive_z();
+      break;
+    default:
+      throw std::runtime_error("Unsupported number of angles");
+      break;
+    }
+  };
+
+  void compute_quadrature_carlson_a4() {
+    const Real mu0 = 1.0 / 3.0;
+    const Real mu1 = std::sqrt(1.0 - 2.0 * mu0 * mu0);  // sqrt(7/9)
+    const Real ww0 = 1.0 / static_cast<Real>(num_angles);
+
+    weights = {ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0,
+               ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0, ww0};
+    mu_x = {-mu0, +mu0, -mu0, +mu0, -mu0, +mu0, -mu0, +mu0,
+            -mu0, +mu0, -mu0, +mu0, -mu0, +mu0, -mu0, +mu0,
+            -mu1, +mu1, -mu1, +mu1, -mu1, +mu1, -mu1, +mu1};
+    mu_y = {-mu0, -mu0, +mu0, +mu0, -mu0, -mu0, +mu0, +mu0,
+            -mu1, -mu1, +mu1, +mu1, -mu1, -mu1, +mu1, +mu1,
+            -mu0, -mu0, +mu0, +mu0, -mu0, -mu0, +mu0, +mu0};
+    mu_z = {-mu1, -mu1, -mu1, -mu1, +mu1, +mu1, +mu1, +mu1,
+            -mu0, -mu0, -mu0, -mu0, +mu0, +mu0, +mu0, +mu0,
+            -mu0, -mu0, -mu0, -mu0, +mu0, +mu0, +mu0, +mu0};
+  };
+
+  void compute_quadrature_single_positive_z() {
+    weights = {1.0};
+    mu_x = {0.0};
+    mu_y = {0.0};
+    mu_z = {1.0};
+  };
+};
