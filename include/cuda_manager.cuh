4b92b6c6b8deac0d50c00eae77c9f1784fc41320
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2025-09-04 21:25:06 +0000

    start using CUDA aware MPI

--- include/cuda_manager.cuh
+++ include/cuda_manager.cuh
@@ -1,5 +1,6 @@
 #pragma once
 #include "grid_cpu.hpp"
+#include "mpi_manager.hpp"
 #include <cuda_runtime.h>
 
 // clang-format off
@@ -27,10 +28,11 @@ template <typename Real> struct CudaManager {
   cudaStream_t stream_ei;
   cudaStream_t stream_ph;
 
-  dim3 grid_dim;
+  dim3 grid_dim, grid_dim_x_margin, grid_dim_y_margin, grid_dim_z_margin;
   dim3 block_dim;
 
   CudaManager(const Grid<Real> &grid) {
+
     CUDA_CHECK(cudaStreamCreate(&stream_ro));
     CUDA_CHECK(cudaStreamCreate(&stream_vx));
     CUDA_CHECK(cudaStreamCreate(&stream_vy));
@@ -45,5 +47,17 @@ template <typename Real> struct CudaManager {
     grid_dim = dim3((grid.i_total + block_dim.x - 1) / block_dim.x,
                     (grid.j_total + block_dim.y - 1) / block_dim.y,
                     (grid.k_total + block_dim.z - 1) / block_dim.z);
+
+    grid_dim_x_margin = dim3((grid.i_margin + block_dim.x - 1) / block_dim.x,
+                             (grid.j_total + block_dim.y - 1) / block_dim.y,
+                             (grid.k_total + block_dim.z - 1) / block_dim.z);
+
+    grid_dim_y_margin = dim3((grid.i_total + block_dim.x - 1) / block_dim.x,
+                             (grid.j_margin + block_dim.y - 1) / block_dim.y,
+                             (grid.k_total + block_dim.z - 1) / block_dim.z);
+
+    grid_dim_z_margin = dim3((grid.i_total + block_dim.x - 1) / block_dim.x,
+                             (grid.j_total + block_dim.y - 1) / block_dim.y,
+                             (grid.k_margin + block_dim.z - 1) / block_dim.z);
   }
 };
