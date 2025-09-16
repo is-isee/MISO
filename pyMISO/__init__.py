f733cb2d369d8c22be1ce62941dc7eb391c30a21
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2025-09-16 16:47:51 +0900

    - pyproject.tomlにpyvistaを追加 - pyMISO.Dataにwrite_scalar_vtkを追加 - data.i_totalなどとするとdata.conf.i_totalを探しに行く設定にしていたが、confには色々なものが描かれるために、不適と判断。confは探しに行かないようにした。
    3D Kelvin Helmholtzと3次元可視化の検討
    Fixes #49

--- pyMISO/__init__.py
+++ pyMISO/__init__.py
@@ -9,16 +9,6 @@ from .data import Data
 from .grid import Grid
 from .mpi import MPI
 from .time import Time
-from .write import write_scalar_vtk
-
-__all__ = [
-    "Conf",
-    "Data",
-    "Grid",
-    "MPI",
-    "Time",
-    "write_scalar_vtk",
-]
 
 try:
     from ._version import version as __version__
--- pyMISO/data.py
+++ pyMISO/data.py
@@ -25,7 +25,9 @@ class Data:
         using np.memmap for efficient access to large binary data.
         """
 
-        self.ro = np.zeros((self.i_size, self.j_size, self.k_size), dtype=self.dtype)
+        self.ro = np.zeros(
+            (self.i_size, self.j_size, self.k_size), dtype=self.conf.dtype
+        )
         self.vx = np.zeros_like(self.ro)
         self.vy = np.zeros_like(self.ro)
         self.vz = np.zeros_like(self.ro)
@@ -37,74 +39,78 @@ class Data:
 
         self.time.load(n_output)
 
-        shape = (self.i_total_local, self.j_total_local, self.k_total_local)
+        shape = (
+            self.conf.i_total_local,
+            self.conf.j_total_local,
+            self.conf.k_total_local,
+        )
         count = np.prod(shape)
 
         for rank in range(self.mpi.n_procs):
             filename = (
-                self.mhd_data_dir
+                self.conf.mhd_data_dir
                 + "mhd."
-                + str(n_output).zfill(self.n_output_digits)
+                + str(n_output).zfill(self.conf.n_output_digits)
                 + "."
-                + str(rank).zfill(self.n_procs_digits)
+                + str(rank).zfill(self.conf.n_procs_digits)
                 + ".bin"
             )
 
             if self.mpi.n_procs > 1:
                 ijk_global = [
                     slice(
-                        self.mpi.coords["x"][rank] * self.i_size_local,
-                        (self.mpi.coords["x"][rank] + 1) * self.i_size_local,
+                        self.mpi.coords["x"][rank] * self.conf.i_size_local,
+                        (self.mpi.coords["x"][rank] + 1) * self.conf.i_size_local,
                     ),
                     slice(
-                        self.mpi.coords["y"][rank] * self.j_size_local,
-                        (self.mpi.coords["y"][rank] + 1) * self.j_size_local,
+                        self.mpi.coords["y"][rank] * self.conf.j_size_local,
+                        (self.mpi.coords["y"][rank] + 1) * self.conf.j_size_local,
                     ),
                     slice(
-                        self.mpi.coords["z"][rank] * self.k_size_local,
-                        (self.mpi.coords["z"][rank] + 1) * self.k_size_local,
+                        self.mpi.coords["z"][rank] * self.conf.k_size_local,
+                        (self.mpi.coords["z"][rank] + 1) * self.conf.k_size_local,
                     ),
                 ]
             else:
                 ijk_global = [
-                    slice(0, self.i_size_local),
-                    slice(0, self.j_size_local),
-                    slice(0, self.k_size_local),
+                    slice(0, self.conf.i_size_local),
+                    slice(0, self.conf.j_size_local),
+                    slice(0, self.conf.k_size_local),
                 ]
 
             ijk_local = [
-                slice(self.i_margin, self.i_total_local - self.i_margin),
-                slice(self.j_margin, self.j_total_local - self.j_margin),
-                slice(self.k_margin, self.k_total_local - self.k_margin),
+                slice(self.i_margin, self.conf.i_total_local - self.i_margin),
+                slice(self.j_margin, self.conf.j_total_local - self.j_margin),
+                slice(self.k_margin, self.conf.k_total_local - self.k_margin),
             ]
 
             with open(filename, "rb") as f:
                 self.ro[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.vx[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.vy[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.vz[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.bx[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.by[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.bz[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.ei[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
                 self.ph[tuple(ijk_global)] = np.fromfile(
-                    f, dtype=self.endian + self.dtype, count=count
+                    f, dtype=self.conf.endian + self.conf.dtype, count=count
                 ).reshape(shape, order="C")[tuple(ijk_local)]
 
         # squeezeはオプション（必要なら）
@@ -118,13 +124,56 @@ class Data:
         self.ei = np.squeeze(self.ei)
         self.ph = np.squeeze(self.ph)
 
-        self.n_output = n_output
+        self.load_n_output = n_output
+
+    def write_scalar_vtk(self, n_output, var, output_path):
+        """
+        Write a scalar variable to a VTK file for visualization using PyVista.
+
+        Parameters
+        ----------
+        self : pyMISO.Data
+            Instance of pyMISO.Data class containing the simulation data.
+        n_output : int
+            The output number to load the data from.
+        var : str
+            The variable name to write (e.g., 'ro', 'vx', 'vy', 'vz', 'bx', 'by', 'bz', 'ei', 'ph').
+        output_path : str
+            The path to save the VTK file.p
+        """
+        import pyvista as pv
+
+        if not isinstance(self, pyMISO.Data):
+            raise TypeError("data must be an instance of pyMISO.Data")
+
+        if not isinstance(n_output, int):
+            raise TypeError("n_output must be an integer")
+
+        if not isinstance(output_path, str):
+            raise TypeError("output_path must be a string")
+
+        if var not in ["ro", "vx", "vy", "vz", "bx", "by", "bz", "ei", "ph"]:
+            raise ValueError(
+                "var must be one of 'ro', 'vx', 'vy', 'vz', 'bx', 'by', 'bz', 'ei', 'ph'"
+            )
+
+        read_flag = True
+        if hasattr(self, "load_n_output"):
+            if n_output == self.load_n_output:
+                read_flag = False
+        if read_flag:
+            self.load(n_output)
+
+        pv_grid = pv.RectilinearGrid(self.x_edge, self.y_edge, self.z_edge)
+        pv_grid.cell_data["scalar"] = getattr(self, var).flatten(order="F")
+
+        pv_grid.save(output_path)
 
     def __getattr__(self, name):
         """
         When an attribute is not found in pyMISO.Data, it is searched in pyMISO.MHD.conf and pyMISO.MHD.grid
         """
-        for obj in [self.conf, self.grid, self.time]:
+        for obj in [self.grid, self.time]:
             if hasattr(obj, name):
                 # Only when the attribute is not a function, return it
                 if not callable(getattr(obj, name)):
