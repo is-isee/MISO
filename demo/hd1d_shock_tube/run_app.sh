0303fdfeede6bf53767d980867e2b1e9281e1937
Haruhisa IIIJIMA <h.iijima0202@gmail.com>
2025-12-08 07:45:20 +0000

    added env file for openmpi config (fix #88)

--- demo/hd1d_shock_tube/run_app.sh
+++ demo/hd1d_shock_tube/run_app.sh
@@ -1,47 +1,22 @@
 #!/usr/bin/env bash
 
+# Error handling
 set -eu
 
-# Default values
-USE_CUDA=false
+# Number of processes
 NUM_PROCS=1
-
-# Analyze command-line arguments
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    --cuda)
-      USE_CUDA=true
-      shift
-      ;;
-    -np)
-      NUM_PROCS="$2"
-      shift 2
-      ;;
-    *)
-      echo "Error: Unknown argument '$1'" >&2
-      echo "Usage: $0 [--cuda] [--np <num>]" >&2
-      exit 1
-      ;;
-  esac
-done
 
 # Define application name
 APP_DIR=$(cd "$(dirname "$0")" && pwd)
 PROJECT_NAME=$(basename "${APP_DIR}")
 APP_NAME="${APP_DIR}/build/${PROJECT_NAME}"
 
-# MPI options for CUDA MPI
-CUDA_MPI_OPTS="--bind-to none"
-CUDA_MPI_OPTS+=" --mca pml ob1"
-CUDA_MPI_OPTS+=" --mca btl tcp,self,vader"
-CUDA_MPI_OPTS+=" --mca coll ^hcoll"
-CUDA_MPI_OPTS+=" --mca osc ^ucx"
+# Load environment variables
+set -a
+source "${APP_DIR}/openmpi_config.env"
+set +a
 
 # Run the application
-if [ "$USE_CUDA" = true ]; then
-  CMD="mpiexec -np ${NUM_PROCS} ${CUDA_MPI_OPTS} ${APP_NAME}"
-else
-  CMD="mpiexec -np ${NUM_PROCS} ${APP_NAME}"
-fi
-echo $CMD
-exec $CMD
+CMD="mpiexec -np ${NUM_PROCS} ${APP_NAME}"
+echo "Executing: ${CMD}"
+exec ${CMD}
