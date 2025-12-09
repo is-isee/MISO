119a383eee00e07f02477db632111dbaf664dc65
Haruhisa IIIJIMA <h.iijima0202@gmail.com>
2025-12-09 03:06:01 +0000

    added confirmation before runnning all demo apps

--- demo/demo_run.sh
+++ demo/demo_run.sh
@@ -10,6 +10,19 @@ THIS_DIR=$(cd "$(dirname "$0")" && pwd)
 # List of demo applications
 source "${THIS_DIR}"/shared/app_list.sh
 
+# Ask user for confirmation
+echo "This process may take a long time. Do you really want to run it? (y/n)"
+read -r answer
+case "$answer" in
+  [Yy]*)
+    echo "Starting execution..."
+    ;;
+  *)
+    echo "Operation cancelled."
+    exit 0
+    ;;
+esac
+
 # Run commands
 set -x
 for APP_NAME in "${APP_LIST[@]}"; do
