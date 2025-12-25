3b04689ae924257323f71d8bc63fd8831e0451ea
iijima <h.iijima0202@gmail.com>
2025-12-25 19:56:06 +0000

    minor fix (compatibility with openmpi HOST macro)

--- core/include/miso/cuda_compat.hpp
+++ core/include/miso/cuda_compat.hpp
@@ -2,12 +2,10 @@
 
 #ifdef USE_CUDA
 #include <cuda_runtime.h>
-#define HOST __host__
 #define DEVICE __device__
 #define HOST_DEVICE __host__ __device__
 #define GLOBAL __global__
 #else
-#define HOST
 #define DEVICE
 #define HOST_DEVICE
 #define GLOBAL
