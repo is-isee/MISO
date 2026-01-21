dcb642e0a1ecc071f89a5329dc30e681bedc7ed4
iijima <h.iijima0202@gmail.com>
2026-01-21 23:57:36 +0000

    moved miso_{build,run,clean}.sh to dev/tools

--- dev/tools/miso_build.sh
+++ dev/tools/miso_build.sh
@@ -7,12 +7,15 @@ set -eu
 # Define directory of this script
 THIS_DIR=$(cd "$(dirname "$0")" && pwd)
 
-# Source and binary directories: MISO core
-MISO_SRC="${THIS_DIR}"/core
-MISO_BIN="${MISO_SRC}"/build
+# Root directory
+MISO_ROOT="${THIS_DIR}/../.."
+
+# Source and binary directories
+MISO_SRC="${MISO_ROOT}/core"
+MISO_BIN="${MISO_SRC}/build"
 
 # Run commands
 set -x
 cmake -B "${MISO_BIN}" -S "${MISO_SRC}"
 cmake --build "${MISO_BIN}" -j
-"${THIS_DIR}"/demo/demo_build.sh
+"${MISO_ROOT}"/demo/demo_build.sh
